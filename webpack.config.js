95f0b9673a312175efd23b323e178c4f61cef0cf
Behn <7383025+BehnH@users.noreply.github.com>
2022-03-26 17:12:29 +0000

    ci: Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'none',
+    target: 'webworker',
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'worker.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader',
+            },
+        ],
+    },
+}
